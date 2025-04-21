5598d6fb130e128c7b48a3d55fb034b5220aef20
techiegirldiaries <divisha.08.9b@gmail.com>
2025-04-21 17:44:08 +0530

    Update dectobinary.c

--- dectobinary.c
+++ dectobinary.c
@@ -1,3 +1,4 @@
+//Convert decimal to Binary using recursion (with 2 logics)
 #include <stdio.h>
 
 int dectobin(int);
@@ -9,24 +10,26 @@ int main()
     printf("The binary number of %d: %d ", n, dectobin(n));
 }
 
-// void dectobin(int n)
-// {
-//     if (n == 1)
-//     {
-//         printf("1");
-//         return;
-//     }
+// First Logic (has been commented for the second one to work as it the return type is different for both func)
+/*void dectobin(int n)
+{
+    if (n == 1)
+    {
+        printf("1");
+        return;
+    }
 
-//     if (n == 0)
-//     {
-//         printf("0");
-//         return;
-//     }
+    if (n == 0)
+    {
+        printf("0");
+        return;
+    }
 
-//     dectobin(n / 2);
-//     printf("%d", n % 2);
-// }
+    dectobin(n / 2);
+    printf("%d", n % 2);
+}*/
 
+//Second Logic
 int dectobin(int n)
 {
     if (n == 0)
